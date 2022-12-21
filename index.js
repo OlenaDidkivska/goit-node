ecd8bb8fc00786bac8879dda25abf71e56ae9f6f
OlenaDidkivska <elenatyutyuma@gmail.com>
2022-12-21 21:49:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,7 @@ const {
   addContact,
 } = require("./contacts.js");
 
-const path = require("path");
-const fs = require("fs");
-
-const contactsPath = path.resolve("./db/contacts.json");
-
-const PORT = 8081;
-
 const { Command } = require("commander");
-const http = require("http");
-
-const requestHandler = async (request, response) => {
-  const contacts = await fs.readFileSync(contactsPath, "utf-8");
-  response.writeHead(200, { ContentType: "text/json" });
-  response.end(contacts);
-};
-
-const server = http.createServer(requestHandler);
-
-server.listen(PORT, (err) => {
-  if (err) {
-    console.error("Error at a server launch", err);
-  }
-  console.log(`Server works at a port ${PORT}`);
-});
 
 const program = new Command();
 program
